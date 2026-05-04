77f7d68c70794d9fc46b2d5e550f29b22ad48270
aslan15 <aguilaraj15@tamu.edu>
2026-05-03 19:18:22 -0500

    instrument definition

---
+++ experiments/2_centralized_vs_decentralized/resources/constellations.py
@@ -0,0 +1,19 @@
+"""
+Constellation Design Script
+
+Generates different satellite constellation designs for comparison between 
+centralized and decentralized approaches in satellite communication networks.
+"""
+
+import numpy as np
+
+# def generate_
+
+if __name__ == "__main__":
+
+    # Define parameters for constellation design
+    num_satellites = 100
+    altitude = 550  # in kilometers
+    inclination = 53  # in degrees
+
+    
