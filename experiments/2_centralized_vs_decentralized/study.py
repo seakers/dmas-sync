99804fa5da0b851035e09989993ead07a37e7a49
aslan15 <aguilaraj15@tamu.edu>
2026-05-23 15:43:46 -0500

    updated reduced time for testing

--- experiments/2_centralized_vs_decentralized/study.py
+++ experiments/2_centralized_vs_decentralized/study.py
@@ -47,7 +47,7 @@ def main_study(sim_cfg: SimulationConfig) -> List[Dict]:
         print(" - Loaded experiment templates from `resources/templates/`")
 
     # duration/step size
-    # duration = 20_000 / 3_600 / 24.0 if sim_cfg.reduced else 1.0  # [days]
+    # duration = 5_500 / 3600 / 24.0 if sim_cfg.reduced else 1.0  # [days]
     duration = 8.0 / 24.0 if sim_cfg.reduced else 1.0  # [days]
     duration = min(duration, 1.0)
     step_size = 10  # [s]
