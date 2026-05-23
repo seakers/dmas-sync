4fb0718faae3f17d30ce878f6f1a46266d4816ff
aslan15 <aguilaraj15@tamu.edu>
2026-05-23 13:14:24 -0500

    added earliest arrival time greedy planner

--- dmas/models/planning/decentralized/earliest.py
+++ dmas/models/planning/decentralized/earliest.py
@@ -33,3 +33,41 @@ class EarliestAccessReactivePlanner(HeuristicInsertionReactivePlanner):
                 -observation_opportunity.min_duration, 
                 observation_opportunity.get_priority()
                 )
+
+class EarliestRequestArrivalPeriodicPlanner(HeuristicInsertionPeriodicPlanner):
+    """ Schedules observations based on the earliest rqeuest time among its tasks """
+    
+    def _calc_heuristic(self,
+                        observation_opportunity : ObservationOpportunity, 
+                        *_ : Any
+                        ) -> tuple:
+        """ Heuristic function to sort observation opportunities by their earliest request arrival time. """
+        # return to sort using: earliest request arrival time >> longest duration >> highest priority
+        arrival_time = min([task.req.t_req for task in observation_opportunity.tasks 
+                            if hasattr(task, 'req')], default=0.0)
+        
+        return (
+                arrival_time,
+                observation_opportunity.accessibility.left, 
+                -observation_opportunity.min_duration, 
+                observation_opportunity.get_priority()
+                )
+    
+class EarliestRequestArrivalReactivePlanner(HeuristicInsertionReactivePlanner):
+    """ Schedules observations based on the earliest rqeuest time among its tasks """
+    
+    def _calc_heuristic(self,
+                        observation_opportunity : ObservationOpportunity, 
+                        *_ : Any
+                        ) -> tuple:
+        """ Heuristic function to sort observation opportunities by their earliest request arrival time. """
+        # return to sort using: earliest request arrival time >> longest duration >> highest priority
+        arrival_time = min([task.req.t_req for task in observation_opportunity.tasks 
+                            if hasattr(task, 'req')], default=0.0)
+        
+        return (
+                arrival_time,
+                observation_opportunity.accessibility.left, 
+                -observation_opportunity.min_duration, 
+                observation_opportunity.get_priority()
+                )
